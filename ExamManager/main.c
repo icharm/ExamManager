dab911259349475a72c7026af112ff987cb0453a
ICHARM <icharm@icharmdemacbook-air.local>
2016-09-02 07:38:20 +0800

    Initial Commit

---
+++ ExamManager/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ExamManager
+//
+//  Created by ICHARM on 16/9/2.
+//  Copyright © 2016年 ICHARM. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
